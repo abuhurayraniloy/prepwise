723bfd347e6c2ca22daf4e514f0085626c75305e
botfigther02 <abuhurayraniloy02@gmail.com>
2025-06-03 00:18:28 +0600

    add: style to hero section

--- components/hero.jsx
+++ components/hero.jsx
@@ -1,23 +1,47 @@
+"use client"
+
 import Link from "next/link"
 import { Button } from "./ui/button"
 import Image from "next/image"
+import { useEffect, useRef } from "react"
 
 const HeroSection = () => {
+
+    const imageRef = useRef(null);
+
+    useEffect(() => {
+        const imageElement = imageRef.current;
+    
+        const handleScroll = () => {
+          const scrollPosition = window.scrollY;
+          const scrollThreshold = 100;
+    
+          if (scrollPosition > scrollThreshold) {
+            imageElement.classList.add("scrolled");
+          } else {
+            imageElement.classList.remove("scrolled");
+          }
+        };
+    
+        window.addEventListener("scroll", handleScroll);
+        return () => window.removeEventListener("scroll", handleScroll);
+      }, []);
+        
   return (
     <section className="w-full pt-36 md:pt-48 pb-10">
-        <div>
-            <div>
-                <h1>
-                    Your AI Career Coach
+        <div className="space-y-6 text-center">
+            <div className="space-y-6 mx-auto">
+                <h1 className="gradient-title text-4xl md:text-5xl lg:text-6xl font-bold">
+                    Your AI Career Coach for 
                     <br />
-                    Professional Guidance for Your Career Journey
+                    Professional Guidance 
                 </h1>
-                <p>
+                <p className="mx-auto max-w-[600px] text-muted-foreground md:text-xl">
                     Advance your career with personalized AI coaching, interview preparation, and resume optimization.
                 </p>
             </div>
 
-            <div>
+            <div className="flex justify-center space-x-4">
                 <Link href="/dashboard">
                     <Button size="lg" className="px-8">
                         Get Started
@@ -32,8 +56,8 @@ const HeroSection = () => {
 
             </div>
 
-            <div>
-                <div>
+            <div className="hero-image-wrapper mt-5 md:mt-0">
+                <div ref={imageRef} className="hero-image">
                     <Image
                         src={"/banner.jpeg"}
                         width= {1280}
