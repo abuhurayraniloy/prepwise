6bf44d3b98f04c2255da834d1b5e6a9981755254
Amit Hassan Joy <sarkeramit88@gmail.com>
2025-06-26 23:01:20 +0600

    fix: logo

--- components/header.jsx
+++ components/header.jsx
@@ -14,7 +14,7 @@ const Header = async() => {
     <header className="fixed top-0 w-full border-b bg-background/80 backdrop-blur-md z-50 supports-[backdrop-filter]:bg-background/60">
         <nav className="container mx-auto px-4 h-16 flex items-center justify-between">
             <Link href='/'>
-                <Image src='/logo.png' alt='PrepWise logo' width={200} height={60} 
+                <Image src='/logo3.jpg' alt='PrepWise logo' width={200} height={60} 
                 className="h-12 py-1 width-auto object-contain" 
             />
             </Link>
