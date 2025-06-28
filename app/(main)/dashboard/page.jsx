20268a0c635232616bd50244795099274ed0ad8d
Amit Hassan Joy <sarkeramit88@gmail.com>
2025-06-28 13:31:23 +0600

    Fix minor issue

--- app/(main)/dashboard/page.jsx
+++ app/(main)/dashboard/page.jsx
@@ -7,11 +7,11 @@ import DashboardView from './_components/dashboard-view';
 const IndustryInsightsPage = async () => {
 
     const { isOnboarded } = await getUserOnboardingStatus();
-    const insights = await getIndustryInsights();
     
         if (!isOnboarded) {
-            redirect("/onboarding");
+          redirect("/onboarding");
         }
+        const insights = await getIndustryInsights();
 
   return (
     <div className='container mx-auto'>
